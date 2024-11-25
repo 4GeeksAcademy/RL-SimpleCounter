37b06cf39ee63f3479a3bbb7e59464c98f00b220
Rodrigo Lau Ramos <darkbass16@gmail.com>
2024-11-25 19:27:04 +0000

    progreso2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Counter</h1>
-
-				<div className="clock col-lg-6 col-12 m-auto">
-					<div className="icon rounded-3 bg-dark bg-gradient border border-dark-subtle">
-						<i class="fa-regular fa-clock"></i>
-					</div>
-					<div className="mil rounded-3 bg-dark bg-gradient border border-dark-subtle">0</div>
-					<div className="cen rounded-3 bg-dark bg-gradient border border-dark-subtle">0</div>
-					<div className="dec rounded-3 bg-dark bg-gradient border border-dark-subtle">0</div>
-					<div className="uni rounded-3 bg-dark bg-gradient border border-dark-subtle">0</div>
+	return function SencondsCounter(props) {
+		return (<div className="text-center">
+			<h1 className="text-center mt-5">Simple Counter</h1>
+			<div className="clock col-lg-6 col-12 m-auto">
+				<div className="icon rounded-3 bg-dark bg-gradient border border-dark-subtle">
+					<i className="fa-regular fa-clock"></i>
 				</div>
-			
-
+				<div className="mil rounded-3 bg-dark bg-gradient border border-dark-subtle">{props.mil % 10}</div>
+				<div className="cen rounded-3 bg-dark bg-gradient border border-dark-subtle">{props.cen % 10}</div>
+				<div className="dec rounded-3 bg-dark bg-gradient border border-dark-subtle">{props.dec % 10}</div>
+				<div className="uni rounded-3 bg-dark bg-gradient border border-dark-subtle">{props.uni % 10}</div>
+			</div>;
 		</div>
-	);
-};
+		);
+	};
+}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,22 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+let counter = 0;
+setInterval(function () {
+	const mil = Math.floor(counter / 1000);
+	const cen = Math.floor(counter / 100);
+	const dec = Math.floor(counter / 10);
+	const uni = Math.floor(counter / 1);
+
+	//render your react application
+	root.render(<Home mil={mil} cen={cen} dec={dec} uni={uni} />);
+
+	root.render(
+		<SecondsCounter mil={mil} cen={cen} dec={dec} uni={uni} />
+	);
+
+	counter++;
+}, 1000);
 
